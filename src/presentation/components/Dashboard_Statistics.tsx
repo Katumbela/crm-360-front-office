38286ac515a62ab578224d3cb6e42f717d4da49e
João Afonso Katombela <ja3328173@gmail.com>
2024-04-15 16:32:52 +0100

    whatsapp socket connection

--- src/presentation/components/Dashboard_Statistics.tsx
+++ src/presentation/components/Dashboard_Statistics.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -10,25 +11,46 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { FaTelegramPlane } from "react-icons/fa";
-
+import { FaWhatsapp } from "react-icons/fa";
 import { QuestionOutlineIcon } from "@chakra-ui/icons";
-import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useAuth } from "../../main/hooks";
+import QRCode from "qrcode.react";
+import io from "socket.io-client";
 
-export const Dashboard_Statistics = () => {
-  const navigate = useNavigate();
+export function Dashboard_Statistics() {
+  const [qrCode, setQrCode] = useState("");
+  const socket = io("http://localhost:3001");
 
-  const handleClick = () => {
-    navigate("/dashboard/email");
+  const account = useSelector(useAuth());
+  const { user } = account;
+
+  const handleConnectWhatsApp = () => {
+    try {
+      socket.emit("connectWhatsApp", user.id);
+      console.log("Conectando ao WhatsApp...");
+    } catch (error) {
+      console.error("Erro ao conectar o WhatsApp:", error);
+    }
   };
+
+  useEffect(() => {
+    socket.on("qrCode", (qrCodeData) => {
+      setQrCode(qrCodeData.qr);
+      console.log(qrCodeData.qr);
+    });
+
+    return () => {
+      socket.off("qrCode"); // Remover o ouvinte quando o componente for desmontado
+    };
+  }, []);
 
   return (
     <Box w="75%" mx="auto">
       <Flex p="4">
-        
         <Spacer />
-        <Button onClick={handleClick} colorScheme="red">
-          New Template
+        <Button onClick={handleConnectWhatsApp} colorScheme="red">
+          Conectar WhatsApp
         </Button>
       </Flex>
 
@@ -48,28 +70,35 @@ export const Dashboard_Statistics = () => {
         my="20"
       >
         <Heading
-  fontSize="2xl"
-  display="flex"
-  alignItems="center"
-  justifyContent="center"
-  gap="2"
-  fontWeight="500"
->
-  <Icon as={FaTelegramPlane} />
-  Crie seu primeiro modelo
-</Heading>
+          fontSize="2xl"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          gap="2"
+          fontWeight="500"
+        >
+          <Icon as={FaWhatsapp} className="text-5xl text-green-600" /> <br />
+        </Heading>
 
-<Text color="gray.700">
-  Crie e edite seus modelos de e-mail (confirmações de pedidos,
-  confirmações de registro, e-mails de automação etc.).
-</Text>
+        {qrCode && (
+          <QRCode
+            value={qrCode}
+            size={256}
+            level={"H"}
+            includeMargin={true}
+          />
+        )}
 
-<Center>
-  <Button borderRadius="50px" maxW="fit-content" colorScheme="red">
-    Criar meu primeiro modelo
-  </Button>
-</Center>
+        <Text color="gray.700">
+          Conecte sua conta para começar a enviar e receber mensagens a partir
+          da Echo Link 360
+        </Text>
 
+        <Center>
+          <Button borderRadius="50px" maxW="fit-content" colorScheme="red">
+            Aguardando!
+          </Button>
+        </Center>
       </Stack>
 
       <Button
@@ -86,8 +115,8 @@ export const Dashboard_Statistics = () => {
         _hover={{ bg: "green.900" }}
       >
         <QuestionOutlineIcon />
-        Support
+        Suporte
       </Button>
     </Box>
   );
-};
+}
