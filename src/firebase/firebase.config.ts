4c57acee1df6bfa1c6de02390397b9a427bed692
Joao Afonso Katombela <ja3328173@gmail.com>
2024-09-22 15:15:16 +0100

    fix: src/firebase/firebase.config.ts. Modificação de código em src/firebase/firebase.config.ts; Criação de uma constante em src/firebase/firebase.config.ts

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,3 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDXKbx634X3Q8dcMiXjHseVzQiAFMvmSdY",
   authDomain: "reputa360.firebaseapp.com",
@@ -7,6 +14,12 @@ const firebaseConfig = {
   appId: "1:37758184858:web:1d40d66b1441f2bf6f9e23",
   measurementId: "G-9MR1JLWZ81"
 };
-  
-  export default firebaseConfig;
-  
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const firestore = getFirestore(app);
