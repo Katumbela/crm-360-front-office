b97e8f7180d6dbbdb4f3a0b3d11810fa7f3a5d1a
Joao Afonso Katombela <ja3328173@gmail.com>
2024-09-22 13:30:26 +0100

    fix: src/firebase/firebase.config.ts. Modificação de código em src/firebase/firebase.config.ts; Criação de uma constante em src/firebase/firebase.config.ts

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDxtRcZzzmBP_J_gJf00xXkGCNCLsIIzOU",
-  authDomain: "crm-r360.firebaseapp.com",
-  projectId: "crm-r360",
-  storageBucket: "crm-r360.appspot.com",
-  messagingSenderId: "467773919506",
-  appId: "1:467773919506:web:45efa822ffb2a0ac3263cb",
-  measurementId: "G-LN3C25H65H"
-  };
+  apiKey: "AIzaSyDXKbx634X3Q8dcMiXjHseVzQiAFMvmSdY",
+  authDomain: "reputa360.firebaseapp.com",
+  projectId: "reputa360",
+  storageBucket: "reputa360.appspot.com",
+  messagingSenderId: "37758184858",
+  appId: "1:37758184858:web:1d40d66b1441f2bf6f9e23",
+  measurementId: "G-9MR1JLWZ81"
+};
   
   export default firebaseConfig;
   
