e69a816f30f3c2402b8dcda005b4f962ba1aa7c7
Joao Afonso Katombela <ja3328173@gmail.com>
2024-09-22 15:15:34 +0100

    fix: src/services/getColaborators.ts. Adição de log no console em src/services/getColaborators.ts; Modificação de código em src/services/getColaborators.ts; Criação de uma constante em src/services/getColaborators.ts

--- src/services/getColaborators.ts
+++ src/services/getColaborators.ts
@@ -1,21 +1,43 @@
 import { UserModel } from "../domain/models";
 import { firestore } from "../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore"; // Importe as funções necessárias
 
-export const fetchCollaborators = async (user: UserModel) => {
+export interface IColaborator {
+    id?: string
+    name: string;
+    bi: string;
+    phone: string;
+    city: string;
+    email: string;
+    password: string;
+}
+
+export const fetchCollaborators = async (user: UserModel): Promise<IColaborator[]> => {
     try {
-        // Referência para o documento na coleção "business" com o ID adequado
-        const businessDocRef = firestore.collection("business").doc(`${user.id.substring(0, 4)}_${user.company_name}`);
+        // Crie uma consulta para a coleção "clientes" onde o empresaId corresponde ao id da empresa logada
+        const clientsRef = collection(firestore, "clientes");
+        const q = query(clientsRef, where("empresaId", "==", `${user.id.substring(0, 4)}_${user.company_name}`));
 
-        // Obtém os dados do documento
-        const businessDocSnapshot = await businessDocRef.get();
-        const businessData = businessDocSnapshot.data();
+        // Obtém os documentos que correspondem à consulta
+        const querySnapshot = await getDocs(q);
 
-        // Verifica se existem colaboradores e os define no estado
-        if (businessData && businessData.collaborators) {
-            return businessData.collaborators
+        // Cria um array para armazenar os colaboradores
+        const collaborators: IColaborator[] = [];
+
+        // Verifica se existem colaboradores
+        if (!querySnapshot.empty) {
+            querySnapshot.forEach((doc) => {
+                const data = doc.data() as IColaborator; // Cast para IColaborator
+                collaborators.push(data); // Adiciona o colaborador ao array
+            });
+        } else {
+            throw new Error("Nenhum colaborador encontrado");
         }
+
+        console.log(collaborators);
+        return collaborators;
     } catch (error: any) {
         console.error("Erro ao buscar colaboradores:", error);
-        return "erro ao pegar colaboradores"
-    } 
+        return []; // Retorna um array vazio em caso de erro
+    }
 }
