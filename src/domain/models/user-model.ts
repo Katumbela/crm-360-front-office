df8396254d3f42e72fdb21dcca09a8c17127a589
Joao Afonso Katombela <ja3328173@gmail.com>
2024-09-22 13:30:25 +0100

    fix: src/domain/models/user-model.ts. Modificação de código em src/domain/models/user-model.ts

--- src/domain/models/user-model.ts
+++ src/domain/models/user-model.ts
@@ -1,20 +1,22 @@
 export interface UserModel {
   id: string;
-  name:  string;
-  email:  string;
-  company_name:  string;
+  name: string;
+  email: string;
+  company_name: string;
   phone: number;
-  website:  string;
-  password:  string;
-  address:  string;
-  team:  string;
-  contacts:  "";
-  city:  string;
-  country:  string;
-  plan:  "Free" | "Basic" | "Premium";
-  online_selling:  "no" | "yes";
+  website: string;
+  password: string;
+  address: string;
+  team: string;
+  contacts: string;
+  city: string;
+  country: string;
+  plan: "Free" | "Basic" | "Premium";
+  online_selling: "no" | "yes";
+  logoFile?: File; // Adicionado para suportar o upload de logo
+  capaFile?: File; // Adicionado para suportar o upload de capa
 }
 
 export type AccountModel = {
-  user:  UserModel;
+  user: UserModel;
 };
